944e6a2c18b740833e7f555959af4d1c34a1aaa2
OldSkyTree <IlyasHassen@gmail.com>
2019-01-07 23:53:17 +0300

    Created a class for storing cells

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,7 +9,7 @@ public class GameController : MonoBehaviour
     public GameObject chipPrefab;
     
     private Dictionary<int, GameObject> playersContainer;
-    private Dictionary<int, GameObject> cellsContainer;
+    private Cell[,] cells = new Cell[7, 7];
     private Dictionary<int, GameObject> chipsContainer;
 
     private bool isInitialized = false;
@@ -30,7 +30,7 @@ public class GameController : MonoBehaviour
             { 2, CreateFigure(2, CreateColorFromIntRGB(225, 204, 79)) },
             { 3, CreateFigure(3, CreateColorFromIntRGB(250, 244, 227)) }
         };
-        cellsContainer = FillField();
+        FillField();
         chipsContainer = CreateChips();
         isInitialized = true;
     }
@@ -65,36 +65,31 @@ public class GameController : MonoBehaviour
         return figure;
     }
 
-    Dictionary<int, GameObject> FillField()
+    void FillField()
     {
-        Dictionary<int, GameObject> dictionary = new Dictionary<int, GameObject>();
-
-        GameObject cell;
         Vector3 cellPosition = new Vector3();
-        Quaternion cellRotation = Quaternion.identity;
-
-        int cellId = 0;
+        
         for (int i = 0; i < 7; i++)
         {
             for (int j = 0; j < 7; j++)
             {
                 if (i % 2 == 0 && j % 2 == 0)
                     continue;
-                float x = (i - 3) * 2.05f;
-                float z = (j - 3) * 2.05f;
+                float z = (3 - i) * 2.05f;
+                float x = (j - 3) * 2.05f;
                 cellPosition = new Vector3(x, 0.1f, z);
                 int cellType = Random.Range(0, 3);
-                cell = CreateCell(cellType);
-                cell.transform.position = cellPosition;
-                dictionary.Add(cellId++, cell);
+                cells[i, j] = CreateCell(cellType);
+                cells[i, j].GetGameObject().transform.position = cellPosition;
+                cells[i, j].GetGameObject().name = "Cell" + (i * 7 + j);
             }
         }
-        return dictionary;
     }
-    GameObject CreateCell(int type)
+    Cell CreateCell(int type)
     {
         Vector3 cellPosition = Vector3.zero;
-        Quaternion cellRotation = Quaternion.Euler(0, Random.Range(0, 4) * 90, 0);
+        int randomTurnCount = Random.Range(0, 4);
+        Quaternion cellRotation = Quaternion.Euler(0, randomTurnCount * 90, 0);
         GameObject cell = Instantiate(cellPrefab, cellPosition, cellRotation);
 
         Vector2[] array = cell.GetComponent<MeshFilter>().mesh.uv;
@@ -103,7 +98,8 @@ public class GameController : MonoBehaviour
             array[i].x += (float)300 / 1024 * type;
         }
         cell.GetComponent<MeshFilter>().mesh.uv = array;
-        return cell;
+
+        return new Cell(type, cell, randomTurnCount);
     }
 
     Dictionary<int, GameObject> CreateChips()
@@ -162,5 +158,98 @@ public class GameController : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+}
+
+class Cell
+{
+    private int up;
+    private int right;
+    private int down;
+    private int left;
+    private GameObject gameObject;
+
+    public Cell(int type, GameObject gameObject)
+    {
+        switch (type)
+        {
+            case 0:
+                up = 0; right = 1; down = 1; left = 0;
+                break;
+            case 1:
+                up = 0; right = 1; down = 1; left = 1;
+                break;
+            case 2:
+                up = 0; right = 1; down = 0; left = 0;
+                break;
+        }
+        this.gameObject = gameObject;
+    }
+    public Cell(int type, GameObject gameObject, int turnCount) : this(type, gameObject)
+    {
+        if (turnCount > 0)
+            RotateRight(turnCount);
+        else if (turnCount < 0)
+            RotateLeft(turnCount);
+    }
+
+    void RotateRight(int turnNumber)
+    {
+        for (int i = 0; i < turnNumber; i++)
+        {
+            int upTemp = up;
+            up = left;
+            left = down;
+            down = right;
+            right = upTemp;
+        }
+    }
+    void RotateLeft(int turnNumber)
+    {
+        for (int i = 0; i < turnNumber; i++)
+        {
+            int upTemp = up;
+            up = right;
+            right = down;
+            down = left;
+            left = upTemp;
+        }
+    }
+
+    public int Up
+    {
+        get { return up; }
+        private set
+        {
+            up = (value > 1 || value < 0) ? 0 : value;
+        }
+    }
+    public int Right
+    {
+        get { return right; }
+        private set
+        {
+            right = (value > 1 || value < 0) ? 0 : value;
+        }
+    }
+    public int Down
+    {
+        get { return down; }
+        private set
+        {
+            down = (value > 1 || value < 0) ? 0 : value;
+        }
+    }
+    public int Left
+    {
+        get { return left; }
+        private set
+        {
+            left = (value > 1 || value < 0) ? 0 : value;
+        }
+    }
+    public GameObject GetGameObject()
+    {
+        return gameObject;
     }
 }
